57cd0aa7a40e1a5e8af0ae50b423dfdf8c96d4fe
Ricardovcn <ricardovitorcn@gmail.com>
2018-11-23 10:15:37 -0200

    Versão Final

--- App.py
+++ App.py
@@ -31,12 +31,14 @@ linguas = {'portugues':10,'english':1}
 
 partidas = {'Arena':435, 'Assalto':445, 'Conquista':426, 'Justa':448, 'Colisão':466 }
 
-elos = {'0' : 'Você ainda não completou as 10 partidas classificatórias nesse modo de jogo.',
+elos = {'0' : 'Não classificado.',
         '1' : 'Bronze V', '2' : 'Bronze IV', '3' : 'Bronze III', '4': 'Bronze II', '5' : 'Bronze I',
         '6': 'Prata V', '7': 'Prata IV', '8': 'Prata III', '9': 'Prata II', '10': 'Prata I',
         '11': 'Ouro V', '12': 'Ouro IV', '13': 'Ouro III', '14': 'Ouro II', '15': 'Ouro I', 
         '16': 'Platina V', '17': 'Platina IV', '18': 'Platina III', '19': 'Platina II', '20': 'Platina I', 
         '21': 'Diamante V', '22': 'Diamante IV', '23': 'Diamante III', '24': 'Diamante II', '25': 'Diamante I', '26': 'Masters I',}
+
+partidas = [ 426, 451, 448, 466 ]
 
 #A assinatura é um hash md5 de um combinado de itens (devId, metodo_da_api, authKey, timestamp)
 def assinatura(devId, metodo, authKey, timestamp):
@@ -67,10 +69,7 @@ def requisicaoDeuses(url, metodo, tipoResposta, devId, authKey, session, lingua)
 def requisicaoPartidas(url, metodo, tipoResposta, devId, authKey, session, nick, queue):
     timestamp = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
     r = requests.get(url+metodo+tipoResposta+'/'+devId+'/'+assinatura(devId, metodo, authKey, timestamp)+'/'+session+'/'+timestamp+'/'+nick+"/"+str(queue))
-    print(url+metodo+tipoResposta+'/'+devId+'/'+assinatura(devId, metodo, authKey, timestamp)+'/'+session+'/'+timestamp+'/'+nick+"/"+str(queue))
     return r.text
-
-
  
 app = Flask(__name__)
 
@@ -107,14 +106,13 @@ def perfil(nick):
   player = requisicaoPlayer(urlPrefixo, 'getplayer', 'json', devId, authKey, session, nick)
   dadosPLayer = json.loads(player)
   if player =='[]':
-    return json.dumps({'erro':'Jogador inexistente'})
+    return render_template('error.html', mensagem="Esse nick corresponde a um jogador inexistente ou com perfil privado.")
   else:
-    # requisicaoPartidas(urlPrefixo, 'getqueuestats​', 'json', devId, authKey, session, nick, partidas['Arena'])
-    # print(requisicaoPartidas)
     dicas = []
     dadoPLayer = dadosPLayer[0]
-    with open('./dicas/dicas.json', 'r') as d:
-      jsond = json.loads(d.read())
+    d = open('./dicas/dicas.json', 'r', encoding='utf-8')
+    jsond = json.loads(d.read())
+    d.close()
     if dadoPLayer['MasteryLevel'] < 75:
       r = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&order=relevance&q=SMITE - EXPLICANDO SOBRE MAESTRIA DE DEUSES&key=AIzaSyDrcUdUEc-7E_QqCh9GTBry4zfh3An5SWg') 
       ytbResp = json.loads(r.text)
@@ -126,20 +124,22 @@ def perfil(nick):
     if dadoPLayer['Tier_Conquest'] < 14:
       r = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&order=relevance&q=SMITE LIGA CONQUISTA - INFORMAÇOES BÁSICAS SOBRE ELO E ORDEM DE PICK&key=AIzaSyDrcUdUEc-7E_QqCh9GTBry4zfh3An5SWg') 
       ytbResp = json.loads(r.text)
-      dicas.append({'titulo': 'Conquista Rankeada', 'texto': 'Vc precisa melhorar', 'video': urlPrefixoYtb+ytbResp['items'][0]['id']['videoId']})
+      dicas.append({'titulo': 'Conquista Rankeada', 'texto': jsond['dicaConquista'], 'video': urlPrefixoYtb+ytbResp['items'][0]['id']['videoId']})
     
     if dadoPLayer['Tier_Joust'] < 14:
       r = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&order=relevance&q=SMITE - DICAS PARA SUBIR DE ELO NA JUSTA 3X3&key=AIzaSyDrcUdUEc-7E_QqCh9GTBry4zfh3An5SWg') 
       ytbResp = json.loads(r.text)
-      dicas.append({'titulo': 'Justa Rankeada (3 vs 3)', 'texto': 'Vc precisa melhorar', 'video': urlPrefixoYtb+ytbResp['items'][0]['id']['videoId']})
+      dicas.append({'titulo': 'Justa Rankeada', 'texto': jsond['dicaJusta'], 'video': urlPrefixoYtb+ytbResp['items'][0]['id']['videoId']})
     
     if dadoPLayer['Tier_Duel'] < 14: 
       r = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&order=relevance&q=SMITE - DICAS PARA SE DAR BEM NO DUELO&key=AIzaSyDrcUdUEc-7E_QqCh9GTBry4zfh3An5SWg') 
       ytbResp = json.loads(r.text)
-      dicas.append({'titulo': 'Justa Rankeada (1 vs 1)', 'texto': 'Vc precisa melhorar', 'video': urlPrefixoYtb+ytbResp['items'][0]['id']['videoId']})
-
+      dicas.append({'titulo': 'Duelo', 'texto': jsond['dicaDuelo'], 'video': urlPrefixoYtb+ytbResp['items'][0]['id']['videoId']})
     return render_template('perfil.html', players=dadosPLayer , elo=elos, dica=dicas)
 
+
+def retornaEficiencia(mediaKills,  mediaMortes, mediaAssists):
+  return float(mediaKills)*1.1+float(mediaAssists)*0.9-float(mediaMortes);
 
 
 @app.route('/player/deus/<id>', methods=['GET', 'POST'])
@@ -149,7 +149,48 @@ def playerGod(id):
   for i in deuses:
     if str(i['id']) == str(id):
       god = i
-  return render_template('deus.html', god = god)
+    
+  #def requisicaoPartidas(url, metodo, tipoResposta, devId, authKey, session, nick, queue):
+  resultados = []
+  for i in partidas:
+    partida =   requisicaoPartidas(urlPrefixo, "getqueuestats", "Json", devId, authKey, session, nickname, i)
+    partida = json.loads(partida)
+    for j in partida:
+     if str(j['GodId']) == str(god['id']):
+       resultados.append(j)
+  resultadoFinal = {'Assists' : 0, 'Deaths': 0, 'Gold': 0, 'Kills': 0, 'Losses': 0, 'Wins': 0}
+  for i in resultados:
+    resultadoFinal['Assists'] += i['Assists']
+    resultadoFinal['Deaths'] += i['Deaths']
+    resultadoFinal['Gold'] += i['Gold']
+    resultadoFinal['Kills'] += i['Kills']
+    resultadoFinal['Losses'] += i['Losses']
+    resultadoFinal['Wins'] += i['Wins']
+  d = open('./dicas/dicas.json', 'r', encoding='utf-8')
+  jsond = json.loads(d.read())
+  d.close()
+  dica=""
+  numPartidas = resultadoFinal['Losses']+resultadoFinal['Wins']
+  videos = []
+  if numPartidas != 0:
+    if retornaEficiencia(resultadoFinal['Kills']/numPartidas,resultadoFinal['Deaths']/numPartidas,resultadoFinal['Assists']/numPartidas)<6.8:
+      if god['Roles']==' Assassino':
+        dica = jsond['dicaAssassino']
+      elif god['Roles']==' Guerreiro':
+        dica = jsond['dicaGuerreiro']
+      elif god['Roles']==' Mago':
+        dica = jsond['dicaMago']
+      elif god['Roles']==' Caçador':
+        dica = jsond['dicaCacador']
+      else:
+        dica = jsond['dicaGuardiao']
+      r = requests.get('https://www.googleapis.com/youtube/v3/search?part=id&order=relevance&q=SMITE Como jogar '+god['Name']+'&key=AIzaSyDrcUdUEc-7E_QqCh9GTBry4zfh3An5SWg') 
+      ytbResp = json.loads(r.text)
+      for i in range(3):
+        videos.append(urlPrefixoYtb+ytbResp['items'][i]['id']['videoId'])
+  return render_template('deus.html', god = god, resultado = resultadoFinal, dica=dica, videos=videos)
+
+
 
 if __name__ == "__main__":
  app.run(debug=True)
---
+++ templates/aprendendo.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+ app.run(debug=True)
